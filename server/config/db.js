70ddcc3397c3dd18488f5d44d1157aa965951393
Hitesh Dharmadhikari <dharmadhikarihitesh@gmail.com>
2021-03-07 16:13:34 +0530

    Changed Mongodb URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229",
+
+  //Server MongoDB deployment
+  //"URI": "mongodb+srv://hitesh:Konvicted4s@cluster0.4mvdo.mongodb.net/books229?retryWrites=true&w=majority",
+  "URI": "mongodb+srv://hitesh:mongodb123@clustermidterm.mubki.mongodb.net/books229?retryWrites=true&w=majority"
 };
