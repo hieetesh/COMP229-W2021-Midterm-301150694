9030e50c241eea1c96f3a0b42fffbacf30a17927
Hitesh Dharmadhikari <dharmadhikarihitesh@gmail.com>
2021-03-07 16:20:47 +0530

    Removing unwanted code and comments

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,5 @@ module.exports = {
   //"URI": "mongodb://localhost/books229",
 
   //Server MongoDB deployment
-  //"URI": "mongodb+srv://hitesh:Konvicted4s@cluster0.4mvdo.mongodb.net/books229?retryWrites=true&w=majority",
   "URI": "mongodb+srv://hitesh:mongodb123@clustermidterm.mubki.mongodb.net/books229?retryWrites=true&w=majority"
 };
